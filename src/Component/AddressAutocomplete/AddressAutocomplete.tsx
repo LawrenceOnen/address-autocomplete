8606b2eb449e6c9276c08b00cf07b46b5ed63b3d
Lawrence Onen Lakuma <lawrenceonen83@gmail.com>
2020-01-15 19:02:54 +0300

    added new files and updates to other files

---
+++ src/Component/AddressAutocomplete/AddressAutocomplete.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react'
+import Styles from './AddressAutocomplete.module.scss';
+
+const AddressAutocomplete: FC = () => <input placeholder="Enter an address" />
+
+export default AddressAutocomplete
